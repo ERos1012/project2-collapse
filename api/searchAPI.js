83d393c51be614c5114d8c507b1b73c59b595fc6
ERos1012 <65523315+ERos1012@users.noreply.github.com>
2023-03-23 13:14:33 -0400

    added sample data and updated searchAPI

--- api/searchAPI.js
+++ api/searchAPI.js
@@ -1,22 +1,27 @@
-const searchInput = document.querySelector('#search');
-const resultsList = document.querySelector('.results');
+const express = require('express');
+const app = express();
 
-searchInput.addEventListener('input', () => {
-  const query = searchInput.value;
+// Define the API endpoint for searching
+app.get('/api/search', (req, res) => {
+  const query = req.query.q;
 
-  // Send the search query to the API endpoint
-  fetch(`/api/search?q=${query}`)
-    .then(response => response.json())
-    .then(results => {
-      // Clear previous results
-      resultsList.innerHTML = '';
-
-      // Add each result to the list
-      results.forEach(result => {
-        const li = document.createElement('li');
-        li.textContent = result.title;
-        resultsList.appendChild(li);
-      });
-    })
-    .catch(error => console.error(error));
+  // Query the database using the search query
+  // and return the results as JSON
+  const results = queryDatabase(query);
+  res.json(results);
 });
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
+
+function queryDatabase(query) {
+  // Implement your database query logic here
+  // and return the matching results as an array of objects
+  return [
+    { id: 1, title: 'Result 1' },
+    { id: 2, title: 'Result 2' },
+    { id: 3, title: 'Result 3' },
+  ];
+}
