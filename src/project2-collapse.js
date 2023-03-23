7c7b14e24ff89a434af1016ed2997a6948db1947
ERos1012 <65523315+ERos1012@users.noreply.github.com>
2023-03-23 12:43:52 -0400

    Added properties and some CSS

--- src/project2-collapse.js
+++ src/project2-collapse.js
@@ -1,11 +1,22 @@
-import { LitElement, html, css } from 'lit';
-
-const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
+import { LitElement, html, css } from "lit";
+const imageURL = new URL(
+  "../assets/placeholder badge icon.png",
+  import.meta.url
+).href;
 
 class Project2Collapse extends LitElement {
   static properties = {
-    header: { type: String },
-  }
+    badgeHeader: { type: String },
+    badgeName: { type: String },
+    badgeUrl: { type: String },
+    badgeDescription: { type: String },
+    badgeImage: { type: String },
+    badgeCreator: { type: String },
+    timeToComplete: { type: String },
+    stepsName: { type: String },
+    stepsDescription: { type: String },
+    stepsTime: { type: String },
+  };
 
   static styles = css`
     :host {
@@ -18,7 +29,7 @@ class Project2Collapse extends LitElement {
       color: #1a2b42;
       max-width: 960px;
       margin: 0 auto;
-      text-align: center;
+      text-align: left;
       background-color: var(--project2-collapse-background-color);
     }
 
@@ -26,63 +37,110 @@ class Project2Collapse extends LitElement {
       flex-grow: 1;
     }
 
-    .logo {
-      margin-top: 36px;
-      animation: app-logo-spin infinite 20s linear;
-    }
-
-    @keyframes app-logo-spin {
-      from {
-        transform: rotate(0deg);
-      }
-      to {
-        transform: rotate(360deg);
-      }
-    }
-
-    .app-footer {
-      font-size: calc(12px + 0.5vmin);
+    .list {
+      display: flex;
+      flex-direction: column;
       align-items: center;
+      justify-content: flex-start;
+      width: 100%;
+      max-width: 960px;
+      margin: 0 auto;
+      padding: 0 1rem;
+      background: #9cb9ee;
     }
 
-    .app-footer a {
-      margin-left: 5px;
+    .list-item {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: flex-start;
+      width: 100%;
+      padding: 1rem;
+    }
+    .list-item-header {
+      display: flex;
+    }
+
+    h2 {
+      font-family: "Roboto", sans-serif;
+      font-size: 1.5rem;
+      margin: 0 0 0 8px;
+    }
+
+    summary {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: flex-start;
+      width: 100%;
+      padding: 1rem;
+      cursor: pointer;
+    }
+
+    .details {
+      font-size: 20px;
+      margin: 16px;
+      border: 3px solid #555;
+    }
+    .details summary {
+      font-size: 20px;
+      font-weight: bold;
+    }
+
+    .badgeImage {
+      width: 50px;
+      height: 50px;
+    }
+
+    input {
+      font-size: 20px;
+      font-weight: bold;
+      border: none;
+      border-bottom: 1px solid black;
+      transition: all 0.3s ease-in-out;
+    }
+    input:focus {
+      border-bottom: 2px solid blue;
+      outline: 1px solid grey;
+      outline-offset: 4px;
+    }
+    input:hover:not(:focus) {
+      border-bottom: 2px solid grey;
+    }
+
+    /** accessibility enhancement to not animate the changes possibly
+    for users that have motion activated disabilities **/
+    @media (prefers-reduced-motion) {
+      input {
+        transition: none;
+      }
     }
   `;
 
   constructor() {
     super();
-    this.header = 'My app';
+    this.badgeHeader = "Badge Header";
+    this.badgeName = "Badge Name";
+    this.badgeUrl = "Badge Url";
+    this.badgeDescription = "Badge Description";
+    this.badgeImage = imageURL;
+    this.badgeCreator = "Badge Creator";
+    this.timeToComplete = "0";
+    this.stepsName = "Steps Name";
+    this.stepsDescription = "Steps Description";
+    this.stepsTime = "0";
   }
 
   render() {
     return html`
       <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
-        <h1>${this.header}</h1>
-
-        <p>Edit <code>src/Project2Collapse.js</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
+        <div class="list">
+          <div class="list-item">
+          </div>
+        </div>
       </main>
-
-      <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
-      </p>
     `;
   }
 }
 
-customElements.define('project2-collapse', Project2Collapse);
+customElements.define("project2-collapse", Project2Collapse);
